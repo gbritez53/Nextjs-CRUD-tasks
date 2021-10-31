0805b1b46a86df20b58fafe7ac7fab814e0ab916
Gabriel Britez <gbritez53@gmail.com>
2021-10-31 12:24:02 -0300

    Added TailwindCss

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,8 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className='bg-gray-800 text-white h-screen'>
       <Head>
         <title>Task Application</title>
         <meta
