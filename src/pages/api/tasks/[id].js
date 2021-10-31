1da989449dbc2ff83f5538d4d62f4bdeeeb19411
Gabriel Britez <gbritez53@gmail.com>
2021-10-31 11:06:05 -0300

    created task model

---
+++ src/pages/api/tasks/[id].js
@@ -0,0 +1,44 @@
+import { connectDB } from 'config/mongoose';
+import Task from 'models/task';
+
+connectDB();
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default async (req, res) => {
+  const {
+    method,
+    body,
+    query: { id },
+  } = req;
+
+  switch (method) {
+    case 'GET':
+      try {
+        const task = await Task.findOne({ _id: id });
+        if (!task) {
+          return res.status(404).json({ message: 'Task not found' });
+        }
+        return res.status(200).json(task);
+      } catch (error) {
+        res.status(400).json({ message: error.message });
+      }
+
+    case 'PUT':
+      try {
+        const task = await Task.findOneAndUpdate({ _id: id }, body);
+        await task.save();
+        return res.status(200).json('Updated Task');
+      } catch (error) {}
+
+    case 'DELETE':
+      try {
+        const deleteTask = await Task.findOneAndDelete({ _id: id });
+        if (!deleteTask) {
+          return res.status(404).json({ message: 'Task not found' });
+        }
+        return res.status(200).json('Deleted Task');
+      } catch (error) {}
+    default:
+      return res.status(400).json({ error: 'Method not allowed' });
+  }
+};
--- src/pages/api/tasks/index.js
+++ src/pages/api/tasks/index.js
@@ -1,7 +1,31 @@
-import { connectDB } from 'src/config/mongoose';
+import { connectDB } from 'config/mongoose';
+import Task from 'models/task';
 
 connectDB();
 
-export default function handler(req, res) {
-  res.status(200).json('tasks');
+export default async function handler(req, res) {
+  const { method, body } = req;
+
+  switch (method) {
+    case 'GET':
+      try {
+        const tasks = await Task.find();
+        return res.status(200).json(tasks);
+      } catch (err) {
+        return res.status(400).json({ message: err.message });
+      }
+
+    case 'POST':
+      try {
+        const newTask = new Task(body);
+        const savedTask = await newTask.save();
+        return res.status(201).json(savedTask);
+      } catch (err) {
+        return res.status(400).json({ message: err.message });
+      }
+
+    default:
+      return res.status(400).json({ error: 'Method not allowed' });
+      break;
+  }
 }
